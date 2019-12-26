a911de3f7783f87428cd1635b3e0b372de35ec10
Kensen <hirohtk@gmail.com>
2019-12-26 15:22:19 -0800

    added animations for the cards to be used in portfolio.  a little buggy right now, working on opacity animations

--- materialize/script.js
+++ materialize/script.js
@@ -1,3 +1,32 @@
-$(document).ready(function(){
+
+$(document).ready(function() {
+    console.log("document ready");
     $('.scrollspy').scrollSpy();
-  });
+    
+    $(".thumbnail").mouseover(function (){
+
+        $(this).addClass("enlarge");
+        $(this).attr("id", "immune");
+        $(".thumbnail:not(#immune)").addClass("fade");
+
+        //need to mark which were faded 
+       
+    }) 
+
+    $(".thumbnail").mouseout(function (){
+        $(this).addClass("deflate");
+        $(this).removeClass("enlarge");
+
+        function deflate() {
+            $(".deflate").removeClass("deflate")
+        }
+
+        setTimeout(deflate, 500);
+        $(this).attr("id", "");
+        //$(".fade").css("-webkit-animation:", "lighten 0.5s");
+        //$(".fade").css("filter", "brightness(100%)");
+        $(".thumbnail").removeClass("fade");
+        
+    }) 
+        
+});
