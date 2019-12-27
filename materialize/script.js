1901f96c777873f30eb6ed2bb56dd5d8606ca0ca
Kensen <hirohtk@gmail.com>
2019-12-26 16:10:27 -0800

    fixed- mouseleave was the appropriate method to use.  mouseleave considered exiting the div which was more appropriate

--- materialize/script.js
+++ materialize/script.js
@@ -5,24 +5,24 @@ $(document).ready(function () {
 
     $(".thumbnail").mouseover(function () {
 
+        // PROBLEM WAS THAT WHENEVER I MOUSE OVER A CHILD ELEMENT IT MOUSES OUT.  CHANGED TO MOUSELEAVE, THINGS SEEM GOOD!
+
         $(".thumbnail").removeClass("brighten");
         $(this).addClass("enlarge");
         $(this).attr("id", "immune");
         $(".thumbnail:not(#immune)").addClass("fade");
 
-    })
-
-    $(".thumbnail").mouseout(function () {
-        $(this).addClass("deflate");
-        $(".thumbnail").addClass("brighten");
-        $(this).removeClass("enlarge");
-        function deflate() {
-            $(".deflate").removeClass("deflate")
-        }
-        setTimeout(deflate, 500);
-        $(this).attr("id", "");
-        $(".thumbnail").removeClass("fade");
-
+        $(this).mouseleave(function () {
+            $(this).addClass("deflate");
+            $(".thumbnail").addClass("brighten");
+            $(this).removeClass("enlarge");
+            function deflate() {
+                $(".deflate").removeClass("deflate")
+            }
+            setTimeout(deflate, 500);
+            $(this).attr("id", "");
+            $(".thumbnail").removeClass("fade");
+        })
     })
 
 });
