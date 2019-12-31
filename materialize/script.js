f5b59f1b279944330a495992935d19df51202ec7
Kensen <hirohtk@gmail.com>
2019-12-31 02:14:23 -0800

    built out comment box feature, need to think about making an api that posts info to mongo when we get there

--- materialize/script.js
+++ materialize/script.js
@@ -2,43 +2,76 @@ $(document).ready(function () {
     console.log("document ready");
     $('.scrollspy').scrollSpy();
 
-    // $("#aboutBlock").removeClass("hidden");
-    // $("#portfolioBlock").removeClass("hidden");
-    // $("#contactBlock").removeClass("hidden");
-
     $("#aboutBlock").addClass("flyInLeft");
-    
 
-    // function fadeIn (input) {
-    //     var fadeIn = $("'#" + input + "'").fadeIn();
-    //     return fadeIn;
-    // }
-
-    // function removeHiddenDelay (input) {
-    //     var remove = $("'#" + input + "'").fadeIn();
-    //     return remove;
-    // }
-
-    // fadeIn("aboutBlock");
-    // setTimeout(removeHiddenDelay("aboutBlock"), 400);
-
-    $(document).scroll(function() {
+    $(document).scroll(function () {
         var y = $(this).scrollTop();
-        // if (y > 0) {
-        //   $('.bottomMenu').fadeIn();
-        // } else {
-        //   $('.bottomMenu').fadeOut();
-        // }
-        console.log(y);
-        
         if (y > 200) {
             $("#portfolioBlock").addClass("flyInRight");
         }
         if (y > 1700) {
             $("#contactBlock").addClass("flyInLeft");
         }
+    });
 
-      });
+    // $(document).on("click", function() { 
+    //     var x = event.clientX;
+    //             var y = event.clientY;
+    //             console.log(x, y);
+    // })
+
+    commentBox();
+
+    function commentBox() {
+        $("#commentOpen").on("click", function () {
+            var row1 = $("<div class='row not'></div>");
+            var row2 = $("<div class='col s12 m5'></div>");
+            var row3 = $("<p id='commentInstructions'>Please click on what you want to comment on</p>")
+            var row4 = $("<div class='input-field col s12'></div>")
+            var appendrow4a = $("<textarea id='textarea2' class='materialize-textarea' data-length='120'></textarea>")
+            var appendrow4b = $("<label for='textarea2'>Comment</label>")
+            var submitComment = $("<button class='btn waves-effect waves-light' id='commentSubmit' type='submit' name='action'>Submit</button>");
+            $("#commentOpen").off("click");
+            $(this).parent().append(row1);
+            row1.append(row2);
+            row2.append(row3);
+            $(this).parent().attr("id", "commentBox");
+    
+            $('html,body').css('cursor','crosshair');
+            
+            function delay() {
+                $(document).one("click", function() {
+                    $('html,body').css('cursor','');
+                    var x = event.clientX;
+                    var y = event.clientY;
+                    console.log(x, y);
+                    row3.empty();
+                    row3.append(row4);
+                    row2.append(submitComment);
+                    row4.append(appendrow4a);
+                    row4.append(appendrow4b);
+                    $(document.elementFromPoint(1637, 425)).click();
+                    
+                    $("#commentSubmit").on("click", function() {
+
+                        // ajax post to my API if hooking to db
+                        // Can't I write an API that simply posts to my sql database?  Just make a website with a post route? 
+
+                        $("#commentOpen").parent().attr("id", "");
+                        $("#commentOpen").parent().children().not("#commentOpen").remove();
+                        // recursion:  had to put this entire thing in a function so that initial click handler could be run again/reset
+                        commentBox();
+                    }
+                    )
+                });
+            }
+            
+            setTimeout(delay, 100);
+            // doing this because the document onclick would fire as soon as the commentopen gets fired
+            
+        });
+    }
+    
 
     $(".thumbnail").mouseover(function () {
 
